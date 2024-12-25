55521de6b8545b7336ecf1a5b5fd6cef3a2af597
Parsa <mehdipourparsa@gmail.com>
2024-12-25 14:55:17 -0500

    feat: added main function

--- gitlitelib.py
+++ gitlitelib.py
@@ -15,4 +15,25 @@ import zlib
 argparser = argparse.ArgumentParser(description="Content tracker")
 
 # To work with sub-commands
-argsybparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check_ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash_object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Command not found.")
